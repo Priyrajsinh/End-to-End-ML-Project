b0928081f34658e8f48ae12e14d4fa1dc0323d1f
Priyrajsinh <priyrajsinh03@gmail.com>
2026-01-02 20:04:47 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import dill
+from sklearn.metrics import r2_score
+
 from src.exception import CustomException
 from src.logger import logger
 
@@ -17,4 +19,23 @@ def save_object(file_path, obj):
 
     except Exception as e:
         logger.error("Error occurred in utils.save_object", exc_info=True)
+        raise CustomException(e, sys)
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for model_name, model in models.items():
+            model.fit(X_train, y_train)
+
+            y_test_pred = model.predict(X_test)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score
+
+        return report
+
+    except Exception as e:
+        logger.error("Error occurred in utils.evaluate_models", exc_info=True)
         raise CustomException(e, sys)
